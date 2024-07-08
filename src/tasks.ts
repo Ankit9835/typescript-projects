dfe18d69787891cccb4fe56237ea189c521f3be5
Ankit9835 <ankitsinha874@gmail.com>
2024-07-08 20:59:06 +0530

    fundamental completer

--- src/tasks.ts
+++ src/tasks.ts
@@ -43,7 +43,6 @@ function renderTask(task:Task): void {
     const taskElement = document.createElement('li')
     taskElement.textContent = task.description
     ulList?.appendChild(taskElement)   
-    
 }
 
 function updatedStorage():void{
